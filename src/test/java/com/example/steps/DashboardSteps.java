101e9e8b90eb4f0664cfd68ef7025dd387537587
Darshana Wijebahu <117095106+ShanCodeWay@users.noreply.github.com>
2024-12-19 08:37:33 +0530

    add some test cases

--- src/test/java/com/example/steps/DashboardSteps.java
+++ src/test/java/com/example/steps/DashboardSteps.java
@@ -1,5 +1,40 @@
 package com.example.steps;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import io.cucumber.java.en.*;
+
 public class DashboardSteps {
-    
+    WebDriver driver;
+
+    @Given("I am on the OrangeHRM dashboard page")
+    public void iAmOnTheDashboardPage() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Documents\\chrome-win64\\chrome-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+        driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+        driver.findElement(By.id("btnLogin")).click();
+    }
+
+    @Then("I should see the {string} widget")
+    public void iShouldSeeTheWidget(String widgetName) {
+        WebElement widget = driver.findElement(By.xpath("//h1[text()='" + widgetName + "']"));
+        Assert.assertTrue(widget.isDisplayed());
+    }
+
+    @When("I click on {string} in the {string} widget")
+    public void iClickOnInWidget(String linkText, String widgetName) {
+        WebElement link = driver.findElement(By.xpath("//a[text()='" + linkText + "']"));
+        link.click();
+    }
+
+    @Then("I should navigate to the {string} page")
+    public void iShouldNavigateToThePage(String pageName) {
+        Assert.assertTrue(driver.getTitle().contains(pageName));
+        driver.quit();
+    }
 }
