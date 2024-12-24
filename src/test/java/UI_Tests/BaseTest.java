cdc4a59842350eaf77b392af03a1b6d69f2708e8
thamodhya <thamodyabandara4@gmail.com>
2024-12-24 19:21:51 +0530

    added files for UI testing

---
+++ src/test/java/UI_Tests/BaseTest.java
@@ -0,0 +1,16 @@
+package UI_Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+
+}
+
